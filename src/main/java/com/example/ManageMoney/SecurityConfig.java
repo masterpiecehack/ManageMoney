c5d6712ce229266b187d89d1e08ea45d6478c30d
Keisuke <tkeisuke318@gmail.com>
2024-03-18 23:31:46 +0900

    20240318 初回コミット ユーザー認証の基本導線疎通済み

---
+++ src/main/java/com/example/ManageMoney/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.ManageMoney;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(auth -> auth
+//                        .antMatchers("/", "/home").permitAll() // "/" と "/home" へのアクセスを許可
+                        .anyRequest().authenticated() // それ以外のリクエストは認証が必要
+                )
+                .formLogin(form -> form
+                        .loginPage("/login") // カスタムログインページのパス
+                        .permitAll() // すべてのユーザーにログインページへのアクセスを許可
+                )
+                .logout(logout -> logout
+                        .permitAll() // すべてのユーザーにログアウトを許可
+                );
+        return http.build();
+    }
+}
---
+++ src/main/java/com/example/ManageMoney/service/UserDetailsServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.ManageMoney.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // ユーザー検索ロジックを実装
+        throw new UsernameNotFoundException("User not found");
+    }
+}
