18ab9d9fba3a9770df01f392e9e8cda5014aab7c
Keisuke <tkeisuke318@gmail.com>
2024-03-25 16:41:18 +0900

    Junit5の初期設定 基本的なテストが1本通ることを確認

---
+++ backend/src/main/java/com/example/ManageMoney/common/JunitCommon.java
@@ -0,0 +1,7 @@
+package com.example.ManageMoney.common;
+
+public class JunitCommon {
+    public String reverse(String str) {
+        return "";
+    }
+}
---
+++ backend/src/main/java/com/example/ManageMoney/controller/JunitController.java
@@ -0,0 +1,8 @@
+package com.example.ManageMoney.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JunitController {
+
+}
